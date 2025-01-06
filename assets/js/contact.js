a7552ed133d4b2bd64180007f7b68c18827b1d78
Asher <asher.haun@outlook.com>
2025-01-06 14:50:06 -0600

    .

--- assets/js/contact.js
+++
@@ -1,8 +0,0 @@
-let showContacts = false;
-$("#contactBubbleLink").onclick = (e => {
-    showContacts = !showContacts
-    $("#contactPage").classList.toggle("open")
-    $("#contactPage").classList.toggle("closed")
-    $("body").classList.toggle("nonscrollable")
-    $("#contactBubbleLink").classList.toggle("open")
-})
